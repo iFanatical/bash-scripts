39f874abee92b47d4fe673fe89ed9354c7e4aa0e
Aaron Bush <theawpportunist@gmail.com>
2026-05-22 12:24:19 -0700

    Added Vortex and Nexus scripts to allow Vortex to work properly on Linux, for modding Skyrim, specifically (collections).

---
+++ nxm-handler.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+STEAM_COMPAT_DATA_PATH="/drives/wd1TB-nvme/SteamLibrary/steamapps/compatdata/489830"
+STEAM_COMPAT_CLIENT_INSTALL_PATH="$HOME/.steam/steam"
+PROTON="$HOME/.steam/steam/compatibilitytools.d/GE-Proton10-34/proton"
+VORTEX_EXE="/drives/wd1TB-nvme/SteamLibrary/steamapps/compatdata/489830/pfx/drive_c/Program Files/Vortex/Vortex.exe"
+
+export STEAM_COMPAT_DATA_PATH
+export STEAM_COMPAT_CLIENT_INSTALL_PATH
+export STEAM_COMPAT_APP_ID=489830
+export SteamAppId=489830
+export SteamGameId=489830
+
+"$PROTON" run "$VORTEX_EXE" -d "$1"
